d3e6e5df3a659ce3ccd19af1f050f1354b7b4777
IcyFate <77408659+IcyFate@users.noreply.github.com>
2023-01-19 17:20:14 +0100

    Dodanie funkcji włączenia i wyłączenia czujnika

--- Miganie_LEDami.ino
+++ Miganie_LEDami.ino
@@ -4,7 +4,7 @@
 
 #define lightSensor A0   // A0 oznacza wejście analogowe do którego podłączony jest czujnik światła
 
-const int resolution = 256;   // Zmienna przechowująca maksymalną wartość dla sygnału PWM
+const int resolution = 255;   // Zmienna przechowująca maksymalną wartość dla sygnału PWM
 
 int lightFromSensor = 0;      // Zmienna przechowująca wartość sygnału analogowego podanego przez czujnik światła
 int redLightConverted = 0;    // Zmienna przechowująca przekonwertowany sygnał z czujnika światła na wartość zmiany jasności światła czerwonego
@@ -19,8 +19,10 @@ int redS = 0;   // Zmienne przechowujące wartości wpisane przez użytkownika p
 int greenS = 0;
 int blueS = 0;
 
+bool sensorOnOff = false;   // Zmienna określająca czy czujnik jest włączony czy wyłączony
+
 void setup() {
-  Serial.begin(115200);   // Prędkość komunikacji między ESP a portem szeregowym
+  Serial.begin(115200);   // Szybkość komunikacji między ESP a portem szeregowym
   analogWriteRange(resolution);   // Zmiana zakresu wyjścia analogowego PWM z 1024 na 256
   analogWrite(redPin, 0);   // Początkowe wyłączenie LED
   analogWrite(greenPin, 0);
@@ -29,25 +31,31 @@ void setup() {
 }
 
 void loop() {
+  
+  if (Serial.available() > 0) {   // Jeśli port szeregowy jest dostępny
+
+    redS = Serial.parseInt();   // Zapisanie wartości podanej przez użytkownika do zmiennej przechowującej wartość danego koloru                                            (pominięcie wszystkich następnych znaków po natrafieniu na znak inny niż liczba, np. przecinek
+    greenS = Serial.parseInt();
+    blueS = Serial.parseInt();
+    sensorOnOff = Serial.parseInt();    // Zapisanie wartości 0 lub 1 określające włączenie lub wyłączenie czujnika
+  }
+
+  if(sensorOnOff == true){      // Wykonuje się gdy czujnik jest włączony
     lightFromSensor = analogRead(lightSensor);    // Odebranie aktualnego stanu z czujnika światła
     redLightConverted = map(lightFromSensor, 0, 1024, redS, 0);     // Przekonwertowanie informacji z czujnika na wartości, o które będzie można skorygować jasność świecenia LEDów
-    greenLightConverted = map(lightFromSensor, 0, 1024, greenS,0);
+    greenLightConverted = map(lightFromSensor, 0, 1024, greenS, 0);
     blueLightConverted = map(lightFromSensor, 0, 1024, blueS, 0);
-    
-    if(Serial.available() > 0){     // Jeśli port szeregowy jest dostępny
-      Serial.println("Podaj wartość R, G, B po przecinku");   // Informacja dla użytkownika
-      
-      redS = Serial.parseInt();   // Zapisanie wartości podanej przez użytkownika do zmiennej przechowującej wartość danego koloru (pominięcie wszystkich następnych znaków po natrafieniu na znak inny niż liczba, np. przecinek
-      greenS = Serial.parseInt();
-      blueS = Serial.parseInt();
-    }
   
     red = redS - redLightConverted;   // Obliczenie aktualnej jasności na podstawie danych sczytanych z czujnika światła
     green = greenS - greenLightConverted;
     blue = blueS - blueLightConverted;
-    
     analogWrite(redPin, red);   // Nadanie odpowiedniej wartości poprzez sygnał PWM do odpowiednich wyprowadzeń R, G i B paska LED
     analogWrite(greenPin, green);
     analogWrite(bluePin, blue);
-    
+  }
+  else{
+     analogWrite(redPin, redS);   // Nadanie odpowiedniej wartości poprzez sygnał PWM do odpowiednich wyprowadzeń R, G i B paska LED
+     analogWrite(greenPin, greenS);
+     analogWrite(bluePin, blueS);
+  }
 }
